2213db583b3db8646e7632d0ff589b77b07041dd
Dharmil Munjapara <131229317+DHARMIL440@users.noreply.github.com>
2024-11-19 23:05:33 +0530

    Delete Expensify directory

--- Expensify/drizzle.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("drizzle-kit").Config} **/
-export default {
-    schema: "@/utils/schema.jsx",  // Updated to use @ alias
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: 'postgresql://neondb_owner:mLd0ROVfSh3B@ep-patient-mode-a5gw41hr.us-east-2.aws.neon.tech/Expenses-Tracker?sslmode=require',
-    }
-};
-
