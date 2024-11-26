4998500561c38509a3a7b19f3be85c2318a07e05
maahishah22 <202201419@daiict.ac.in>
2024-11-26 19:58:57 +0530

    Update BudgetItem.jsx

--- expense-tracker/app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
+++ expense-tracker/app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
@@ -15,12 +15,12 @@ function BudgetItem({ budget }) {
             <h2 className='font-semibold text-gray-500'>{budget?.totalItem} Items</h2>
           </div>
         </div>
-        <h2 className='font-bold text-[#3498db] text-lg'>${budget?.totalSpend || 0}</h2>
+        <h2 className='font-bold text-[#3498db] text-lg'>Rs.{budget?.totalSpend || 0}</h2>
       </div>
       <div className='mt-5'>
         <div className='flex justify-between mb-3 text-xs text-slate-400'>
-          <h2>${budget?.totalSpend || 0} Spend</h2>
-          <h2>${budget?.amount - (budget?.totalSpend || 0)} Remaining</h2>
+          <h2>Rs.{budget?.totalSpend || 0} Spend</h2>
+          <h2>Rs.{budget?.amount - (budget?.totalSpend || 0)} Remaining</h2>
         </div>
         <div className='w-full bg-[#2a2a2a] h-2 rounded-full'>
           <div className='bg-[#3498db] h-2 rounded-full' style={{ width: `${calculateProgressPerc()}%` }}></div>
